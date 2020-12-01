4fd4f657d9de5cc01f6b9b1b42e592596fa427c7
Chris Lusted <kipl20@hotmail.com>
2020-12-01 12:22:44 +0000

    pdf-lib test

--- pages/api/test-api.ts
+++ pages/api/test-api.ts
@@ -4,6 +4,7 @@ import getScreenshot from '../../screenshot';
 export default async (_req: NextApiRequest, res: NextApiResponse) => {
   const file = await getScreenshot();
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'image/png');
+  // res.setHeader('Content-Type', 'image/png');
+  res.setHeader('Content-Type', 'application/pdf')
   res.end(file);
 }
--- screenshot.ts
+++ screenshot.ts
@@ -1,5 +1,7 @@
 import chrome from "chrome-aws-lambda";
 import puppeteer from "puppeteer-core";
+import { PDFDocument } from "pdf-lib";
+import fetch from "isomorphic-unfetch";
 
 const isProd = process.env.NODE_ENV === "production";
 
@@ -28,7 +30,53 @@ export default async () => {
   const page = await browser.newPage();
   await page.goto("https://apple.com");
   const file = await page.screenshot({ type });
-  await browser.close();
+  // create pdf from the resulting screenshot
+  const pdf = await page.pdf({
+    format: "A4",
+    margin: {
+      top: "45mm",
+      right: "22mm",
+      bottom: "40mm",
+      left: "22mm",
+    },
+  });
+  // await browser.close();
 
-  return file;
+  const url =
+    "https://raw.githubusercontent.com/Hopding/pdf-lib/master/assets/pdfs/examples/create_document.pdf";
+  const arrayBuffer = await fetch(url).then((res) => res.arrayBuffer());
+  const designDoc = await PDFDocument.load(arrayBuffer);
+  const topLayerPDF = await PDFDocument.load(pdf);
+
+  const pdfDoc = await PDFDocument.create();
+
+  const [template] = await pdfDoc.embedPdf(designDoc);
+
+  const pages = topLayerPDF.getPages();
+
+  for (let idx = 0; idx < pages.length; idx++) {
+    const page = pages[idx];
+
+    const pageContent = await pdfDoc.embedPage(page);
+    const newPage = pdfDoc.addPage();
+
+    newPage.drawRectangle({
+      width: page.getWidth(),
+      height: page.getHeight(),
+    });
+
+    newPage.drawPage(template, {
+      x: 0,
+      y: 0,
+    });
+
+    newPage.drawPage(pageContent, {
+      x: 0,
+      y: 0,
+    });
+  }
+
+  const pdfBytes = await pdfDoc.save();
+
+  return Buffer.from(pdfBytes);
 };
