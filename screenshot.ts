408a1dde74f9847ca3d6d2d368016d67ef8b02e6
Chris Lusted <kipl20@hotmail.com>
2020-12-01 12:08:54 +0000

    more checks

--- screenshot.ts
+++ screenshot.ts
@@ -1,33 +1,34 @@
-import chrome from 'chrome-aws-lambda';
-import puppeteer from 'puppeteer-core';
+import chrome from "chrome-aws-lambda";
+import puppeteer from "puppeteer-core";
 
-const isDev = true;
+const isProd = process.env.NODE_ENV === "production";
 
-const LOCAL_CHROME_PATH = process.platform === 'win32'
-  ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
-  : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+const LOCAL_CHROME_PATH =
+  process.platform === "win32"
+    ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+    : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
 
 export default async () => {
-  const type = 'png';
+  const type = "png";
 
-  const puppeteerOptions = isDev
+  const puppeteerOptions = isProd
     ? {
-        args: [],
-        executablePath: LOCAL_CHROME_PATH,
-        headless: true
-      }
-    : {
         args: chrome.args,
         executablePath: await chrome.executablePath,
         headless: chrome.headless,
+      }
+    : {
+        args: [],
+        executablePath: LOCAL_CHROME_PATH,
+        headless: true,
       };
 
   const browser = await puppeteer.launch(puppeteerOptions);
 
   const page = await browser.newPage();
-  await page.goto('https://apple.com');
+  await page.goto("https://apple.com");
   const file = await page.screenshot({ type });
   await browser.close();
 
   return file;
-}
+};
